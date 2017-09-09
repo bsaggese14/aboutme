15e7c520cf1cbc95fb0c4b377bbc9e3002b0f542
bryan <bsaggese14@aol.com>
2017-09-09 16:23:06 -0400

    updated

---
+++ main.js
@@ -0,0 +1,48 @@
+function loadHome() {
+	document.getElementById("content").innerHTML = "<h2>Major</h2>"+
+				"<p>Information Science</p>"+
+			"<h2>College</h2>"+
+				"<p>CALS</p>"+
+			"<h2>Graduation Year</h2>"+
+				"<p>2017</p>"+
+			"<h2>Hometown</h2>"+
+				"<p>Baldwin, NY.</p>"+
+			"<h2>Curennt Residence</h2>"+
+				"<p>222 Dryden Rd, Ithaca, NY.</p>"+
+			"<h2>Date of Birth</h2>"+
+				"<p>July 30, 1996</p>";
+}
+function loadInspiration() {
+	document.getElementById("content").innerHTML = "<h2>Mary Engelbreit </h2>"+
+				"<p>'If you don't like something change it; if you can't change it, change the way you think about it.'</p>"+
+			"<h2>Thomas Jefferson </h2>"+
+				"<p>'I find that the harder I work, the more luck I seem to have.'</p>"+
+			"<h2>Thomas Alva Edison </h2>"+
+				"<p>'I have not failed. I've just found 10,000 ways that won't work.'</p>"+
+			"<h2>Samuel Johnson </h2>"+
+				"<p>'The true measure of a man is how he treats someone who can do him absolutely no good.'</p>"+
+			"<h2>Anonymous </h2>"+
+				"<p>'The grass is not greener on the other side... it is greener where you water it.'</p>";
+}
+function loadInterests() {
+	document.getElementById("content").innerHTML = "<h2>Programming </h2>"+
+			"<p>I enjoy working on personal projects such as this website. I also enjoy building iOS apps.</p>"+
+			"<h2>Snowboarding </h2>"+
+			"<p>Nothing better than hitting the slopes after a some fresh powder drops. You might find me at Greek Peak, Windham, or Hunter.</p>"+
+			"<h2>Basketball</h2>"+
+			"<p>I'd like to think that if I grew to be over 6' I would have had a shot at professional basketball. JK, but my favorite team is the NY Knicks.</p>"+
+			"<h2>Boating</h2>"+
+			"<p>Growing up on the south shore of Long Island has been a blessing. My summers are filled with boats, jetskis, and watersports.</p>";
+}
+function loadContact() {
+	document.getElementById("content").innerHTML = "<h2>Mary Engelbreit </h2>"+
+				"<p>'If you don't like something change it; if you can't change it, change the way you think about it.'</p>"+
+			"<h2>Thomas Jefferson </h2>"+
+				"<p>'I find that the harder I work, the more luck I seem to have.'</p>"+
+			"<h2>Thomas Alva Edison </h2>"+
+				"<p>'I have not failed. I've just found 10,000 ways that won't work.'</p>"+
+			"<h2>Samuel Johnson </h2>"+
+				"<p>'The true measure of a man is how he treats someone who can do him absolutely no good.'</p>"+
+			"<h2>Anonymous </h2>"+
+				"<p>'The grass is not greener on the other side... it is greener where you water it.'</p>";
+}
